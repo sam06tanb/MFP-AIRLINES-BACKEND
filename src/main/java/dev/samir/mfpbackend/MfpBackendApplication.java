90d7096739c19574dc03ecd169595b26599db903
sam06tanb <samirtajra53@gmail.com>
2025-02-05 01:18:09 -0300

    feat(db): conexao com banco de dados postgres usando docker e dbeaver

---
+++ src/main/java/dev/samir/mfpbackend/MfpBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.samir.mfpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MfpBackendApplication.class, args);
+    }
+
+}
